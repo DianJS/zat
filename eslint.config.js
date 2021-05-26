a8d55e7ccff4db6198ffca57afd930cecfbf17b7
三少 <sanshao@dian.so>
2021-05-26 18:08:19 +0800

    chore: 添加eslint stylelint

---
+++ eslint.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: ['./node_modules/@zatjs/eslint/index.js'],
+  globals: {},
+  settings: {
+    react: {
+      pragma: 'React',
+      version: '17.0',
+    },
+  },
+  rules: {},
+}
---
+++ stylelint.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  extends: ['@zatjs/stylelint'],
+  rules: {},
+}
